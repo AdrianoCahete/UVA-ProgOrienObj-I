dc6416fe7f5bf9b95af4c257b7da8777d649c25a
Adriano Cahete <me@adrianocahete.dev>
2024-09-06 18:38:25 -0300

    feat(ava1): melhorando os resultados dos inputs

--- AVA1/src/Computador.java
+++ AVA1/src/Computador.java
@@ -1,10 +1,12 @@
 /**
  * Classe que representa um Computador.
- * Possui cinco atributos: marca, 2, 3, 4, e 5. // TODO: Colocar os atributos corretamente
- * Contém dois construtores e métodos de acesso (setters e getters), além de um método para imprimir os atributos.
+ * Possui cinco atributos: marca, 2, 3, 4, e 5. // TODO: Colocar os atributos
+ * corretamente
+ * Contém dois construtores e métodos de acesso (setters e getters), além de um
+ * método para imprimir os atributos.
  */
 public class Computador {
-    
+
     // Atributos
     private String marca;
 
@@ -29,6 +31,6 @@ public class Computador {
 
     // Método para imprimir os atributos
     public void imprimir() {
-        System.out.println("Marca: " + this.marca);
+        System.out.println(" Marca: " + this.marca);
     }
 }
--- AVA1/src/Main.java
+++ AVA1/src/Main.java
@@ -2,8 +2,10 @@ import java.util.Scanner;
 
 /**
  * Classe instancia quatro objetos da classe Computador.
- * Dois objetos são criados com o construtor default e dois com o construtor com param.
- * Solicita dados ao usuário para preencher os atributos e exibe os valores com o método imprimir.
+ * Dois objetos são criados com o construtor default e dois com o construtor com
+ * param.
+ * Solicita dados ao usuário para preencher os atributos e exibe os valores com
+ * o método imprimir.
  */
 public class Main {
     public static void main(String[] args) {
@@ -13,23 +15,25 @@ public class Main {
 
         for (int i = 0; i < 2; i++) {
             computadores[i] = new Computador();
-            System.out.println("Preencha os dados do computador " + (i + 1) + " (Construtor Default):");
+            System.out.println(
+                    "\n [" + (i + 1) + "] Preencha os dados do computador " + (i + 1) + " (Default):");
 
-            System.out.print("Marca: ");
+            System.out.print(" Marca: ");
             computadores[i].setMarca(scanner.nextLine());
         }
 
         for (int i = 2; i < 4; i++) {
-            System.out.println("Preencha os dados do computador " + (i + 1) + " (Construtor Parametrizado):");
-            System.out.print("Marca: ");
+            System.out.println(
+                    "\n [" + (i + 1) + "] Preencha os dados do computador " + (i + 1) + " (Parametrizado):");
+            System.out.print(" Marca: ");
             String marca = scanner.nextLine();
 
             computadores[i] = new Computador(marca);
         }
 
-        System.out.println("\nDados dos Computadores:");
+        System.out.println("\n-------------------\n[*] Dados dos Computadores \n");
         for (int i = 0; i < 4; i++) {
-            System.out.println("Computador " + (i + 1) + ":");
+            System.out.println(" [" + (i + 1) + "] Computador " + (i + 1));
             computadores[i].imprimir();
             System.out.println();
         }
