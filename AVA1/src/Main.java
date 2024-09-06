131d650241179ffa01570b85dab31a616bf28aef
Adriano Cahete <me@adrianocahete.dev>
2024-09-06 17:54:37 -0300

    feat(ava1): instanciando scanner que esqueci

--- AVA1/src/Main.java
+++ AVA1/src/Main.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
  */
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
         Computador[] computadores = new Computador[4];
 
