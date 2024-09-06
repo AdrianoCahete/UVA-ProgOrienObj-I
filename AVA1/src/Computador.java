af91c257e9d456c0ec735e7c5ec681972b1ed961
Adriano Cahete <me@adrianocahete.dev>
2024-09-06 19:29:07 -0300

    feat(ava1): adicionando os atributos na classe

--- AVA1/src/Computador.java
+++ AVA1/src/Computador.java
@@ -1,23 +1,34 @@
 /**
  * Classe que representa um Computador.
- * Possui cinco atributos: marca, 2, 3, 4, e 5. // TODO: Colocar os atributos
- * corretamente
- * Contém dois construtores e métodos de acesso (setters e getters), além de um
- * método para imprimir os atributos.
+ * Possui cinco atributos: marca, cpu, ram, disk, e os.
+ * Contém dois construtores e métodos de acesso (setters e getters), além de um método para imprimir os atributos.
  */
-public class Computador {
 
+public class Computador {
+    
     // Atributos
     private String marca;
+    private String cpu;
+    private int ram;
+    private int disk;
+    private String OS;
 
-    // Construtor default
+    // Construtor default (sem parâmetros)
     public Computador() {
         this.marca = "Desconhecida";
+        this.cpu = "Desconhecido";
+        this.ram = 0;
+        this.disk = 0;
+        this.OS = "Desconhecido";
     }
 
     // Construtor com parâmetros
-    public Computador(String marca) {
+    public Computador(String marca, String cpu, int ram, int disk, String OS) {
         this.marca = marca;
+        this.cpu = cpu;
+        this.ram = ram;
+        this.disk = disk;
+        this.OS = OS;
     }
 
     // Getters e Setters
@@ -29,8 +40,44 @@ public class Computador {
         this.marca = marca;
     }
 
+    public String getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(String cpu) {
+        this.cpu = cpu;
+    }
+
+    public int getRam() {
+        return ram;
+    }
+
+    public void setRam(int ram) {
+        this.ram = ram;
+    }
+
+    public int getDisk() {
+        return disk;
+    }
+
+    public void setDisk(int disk) {
+        this.disk = disk;
+    }
+
+    public String getOS() {
+        return OS;
+    }
+
+    public void setOS(String OS) {
+        this.OS = OS;
+    }
+
     // Método para imprimir os atributos
     public void imprimir() {
         System.out.println(" Marca: " + this.marca);
+        System.out.println(" CPU: " + this.cpu);
+        System.out.println(" RAM: " + this.ram + " GB");
+        System.out.println(" Disk: " + this.disk + " GB");
+        System.out.println(" Sistema Operacional: " + this.OS);
     }
 }
